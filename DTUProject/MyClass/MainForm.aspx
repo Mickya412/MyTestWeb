348b4f5435112424486bf68636287758ef7d4552
Trouver <1015347509@qq.com>
2019-08-30 09:36:59 +0800

    Add files via upload

---
+++ DTUProject/MyClass/MainForm.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="DTUProject.MyClass.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>温湿度数据</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GradView1" runat="server" PageSize="20" AllowPaging="true" 
+                AutoGenerateColumns="false" DataKeyNames="id">
+                <Columns>
+                    <asp:TemplateField HeaderText="时间">
+                        <ItemTemplate>
+                            <%# Eval("addtime") %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="温度℃">
+                        <ItemTemplate>
+                            <%# Eval("temp") %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="湿度%">
+                        <ItemTemplate>
+                            <%# Eval("humidity") %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+
+
+        <%--<div>
+            <h1 style="background-color:red;color:aqua;font-family:Verdana;font-size:40px;">标题1</h1>
+            <h2 style="background-color:springgreen">标题2</h2>
+            <h3 style="background-color:blue">标题3</h3>
+            <a href="http://www.baidu.com"> 百度连接</a>
+        </div>
+        <div>
+            <table border="1" style="width:auto">
+                <tr>
+                    <th style="align-content:center">时间</th>
+                    <th style="align-content:center">温度/℃</th>
+                    <th style="align-content:center">湿度/%</th>
+                </tr>
+                <tr>
+                    <th style="align-content:center">2019-08-23 10:20:55</th>
+                    <th style="align-content:center">26</th>
+                    <th style="align-content:center">65</th>
+                </tr>
+            </table>
+        </div>--%>
+
+        <asp:Button ID="Btn" runat="server" Text="Button" OnClick="Btn_Click"/>
+
+    </form>
+
+    <%-- Request对象可用于从表单取回用户信息 --%>
+    <form method="get" action="MainForm.aspx">
+        <p> First Name：<input type="text" name="fname" /></p>
+        <p>Last Name：<input type="text" name="lname" /></p>
+        <input type="submit" value="Submit" />
+    </form>
+
+    
+</body>    
+</html>
---
+++ DTUProject/MyClass/MainForm.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using MySql.Data.MySqlClient;
+using System.Web.Configuration;
+using System.Text.RegularExpressions;
+using System.Data;
+
+namespace DTUProject.MyClass
+{
+    public partial class MainForm : System.Web.UI.Page
+    {
+        MySqlConnection conn = CreateConn();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //添加新的记录
+            //AddNewRecord("34", "68", DateTime.Now.ToString());
+
+            if (!IsPostBack)
+            {
+                GetDataBind();
+            }
+
+            
+        }
+
+        //创建与数据库的连接
+        public static MySqlConnection CreateConn()
+        {
+            string _conn = WebConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+            MySqlConnection conn = new MySqlConnection(_conn);
+            return conn;
+        }
+
+        //将数据绑定到网页上
+        void GetDataBind()
+        {
+            conn.Open();
+            DataSet ds = QueryDataSet("select * from temp");
+            GradView1.DataSource = ds;
+            GradView1.DataBind();
+            conn.Close();
+        }
+
+        public DataSet QueryDataSet(string sqlyj)
+        {
+            //安全处理，拒绝对数据库操作危险的语句，只处理查询操作
+            Regex delete = new Regex("delete");
+            //转换为小写
+            Match deletemat = delete.Match(sqlyj.ToLower());
+            if (deletemat.Success)
+            {
+                sqlyj = "";
+            }
+
+            Regex update = new Regex("update");
+            Match updatemat = update.Match(sqlyj.ToLower());
+            if (updatemat.Success)
+            {
+                sqlyj = "";
+            }
+
+            Regex insert = new Regex("insert");
+            Match insertmat = insert.Match(sqlyj.ToLower());
+            if (insertmat.Success)
+            {
+                sqlyj = "";
+            }
+
+            Regex database = new Regex("database");
+            Match databasemat = database.Match(sqlyj.ToLower());
+            if (databasemat.Success)
+            {
+                sqlyj = "";
+            }
+
+            Regex table = new Regex("table");
+            Match tablemat = table.Match(sqlyj.ToLower());
+            if (tablemat.Success)
+            {
+                sqlyj = "";
+            }
+
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(sqlyj, conn);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds, "a6");
+            dataAdapter.Dispose();
+            return ds;
+        }
+
+        void AddNewRecord(string temp, string humidity, string addtime)
+        {
+            conn.Open();
+            string sql = string.Format("insert into temp(temp,humidity,addtime) values ({0},{1},' {2} ');", temp, humidity, addtime);
+            MySqlCommand sqlcmd = new MySqlCommand(sql, conn);
+            try
+            {
+                sqlcmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            conn.Close();
+        }
+
+        protected void Btn_Click(object sender, EventArgs e)
+        {
+            
+            Response.Write(Request.QueryString);
+        }
+    }
+}
